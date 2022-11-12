d7f97a72544110a35bce8e818cb86ae79d60b47c
Johannes Chorzempa <jchorzempa@posteo.org>
2022-11-12 09:01:53 +0100

    Done

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,8 +7,9 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const fullscreen = player.querySelector('.fullscreen');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 // Build functions
 
@@ -24,6 +25,56 @@ function togglePlay() {
 	// }
 }
 
+function updateButton() {
+	const icon = this.paused ? '►' : '❚ ❚';
+	// "this" can be used because the function is bound to video
+	toggle.textContent = icon;
+}
+
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+	// .currentTime is HTMLMediaElement. this is referencing the object that called the function. Dataset refers to the "data-" part as CSS property, and skip is the part "-skip" in "data-skip"
+}
+
+function handleRangeUpdate() {
+	video[this.name] = this.value;
+	// video = the HTMLMediaElement, with HTMLMediaElement.volume and HTMLMediaElement.playbackRate as instance properties
+}
+
+function handleProgress() {
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}% `;
+}
+
+function scrub(e) {
+	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+	console.log(e);
+}
+
+function fullScreen() {
+	video.requestFullscreen();
+}
+
+// requestFullscreen API
+
 // Hook up event listeners
 
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach((skipButton) => skipButton.addEventListener('click', skip));
+
+ranges.forEach((range) => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach((range) => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+// if mousedown is true (so, if the mouse is down) then it can move on to scrub. Otherwise it won't
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
+fullscreen.addEventListener('click', fullScreen);
